daeb2cd5cac1630dc691d7860a73176b11a45b2c
karthikgittest <39758437+karthikgittest@users.noreply.github.com>
2019-07-20 11:03:37 +0530

    changes

--- Tests/headless.bat
+++ Tests/headless.bat
@@ -1,4 +1,4 @@
 ECHO starting batch run
-cd D:\Assessment-Portal\Tests
+cd C:\vsts-agent-win-x64-2.150.3\_work\2\a\Tests
 ECHO Win7/Chrome
-start pybot -v REMOTE_URL:https://assessments-uat.ccl.org -T    headless.robot
+start pybot -v REMOTE_URL:https://assessments-uat.ccl.org -T -d C:\vsts-agent-win-x64-2.150.3\_work\2\a\Tests\Output headless.robot
